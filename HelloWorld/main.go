d43a1ddf9208d4687751753c245ee48bacd87dc9
Alwyn-Tan <2660412180@qq.com>
2025-02-04 11:24:06 +0800

    add: HttpServer

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	// Register a handler for the specific path
+	// http.ResponseWritter configures http response
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World! You have requested: %s\n", r.URL.Path)
 	})
